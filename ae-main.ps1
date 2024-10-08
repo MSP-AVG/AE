e77b98b627272c879c64905d454c3e65bb1a267e
MSP-AVG <peter.mekes@avitgroup.com>
2024-10-08 13:29:52 +0200

    Update ae-main.ps1

--- ae-main.ps1
+++ ae-main.ps1
@@ -5,12 +5,12 @@ Creates Setup Complete Files
 
 
 Set-ExecutionPolicy Bypass -Force
-iex (https://raw.githubusercontent.com/MSP-AVG/AE/refs/heads/main/ae-ap-menu.ps1)
+iex (irm https://raw.githubusercontent.com/MSP-AVG/AE/refs/heads/main/ae-ap-menu.ps1)
 
 Write-Host -Foreground Red $GroupTag
 sleep -Seconds 3
 
-iex (https://raw.githubusercontent.com/MSP-AVG/AE/refs/heads/main/ae-functions.ps1)
+iex (irm https://raw.githubusercontent.com/MSP-AVG/AE/refs/heads/main/ae-functions.ps1)
 
 #++++++++++++++++++++++++++++++
 # Functions were here !!
